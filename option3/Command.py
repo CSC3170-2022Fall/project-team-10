af2b282d723f987d8df15477e6dca7cf06e855b3
120090311 <120090311@link.cuhk.edu.cn>
2022-12-06 17:19:32 +0800

    details of "command" and "condition"

--- option3/Command.py
+++ option3/Command.py
@@ -1,7 +1,18 @@
 class Command:
-    type = ""
-    name = [] # a list for name of tables
-    rows = [] # for insert
-    column = [] # a list of string that store the column name
-    condition = [] # a list of condition
+    type = "" #possibile values: 'create table' 'create table as' 'load' 'store' 'insert into' 'print' 'quit' 'exit' 'select'
+    name = [] # a list for name of tables, all elements are in form of string
+    # e.g. ['student', 'instructor']
+    #for load commamd it is <name>, only one element
+    #for create table, create table as, store, insert into and print command, it is <table name>, only one element
+    #for select command, it is <table name> after "for", may be more than one
+    # if the command is create table as, then the first element is used for <table name> after create table, the rest is used for <table name> in <select clause>
+    rows = [] #used for insert command, refers to <literal>, in form of string even the original data is number
+    # e.g. if the column of the table is ['instructor_id', 'salary', 'instructor_name']
+    # then one instance may be ['123', '12000.00', 'Tom']
+    column = [] # a list of string that store the column name, in form of string
+    # used for select and create command, refers to <column name>
+    #e.g. ['studemt_id', 'student_name', 'major']
+    condition = [] # a list of condition classes
+    #used for select command <condition caluse>
+    #the logical relationship between conditions is "and"
     
--- option3/Condition.py
+++ option3/Condition.py
@@ -1,6 +1,29 @@
 class Condition:
     left = ""
-    left_type = "" # number / string / column
-    relation # > < >= <= == !=
+    left_type = "" # 'int'/ 'float' / 'string' / 'column'
+    relation = "" # > < >= <= == !=
     right = ""
     right_type = ""
+    
+#e.g if the condition is student_name == 'Tom'
+# left = "student_name"
+# left_type = "column" 
+# relation = "==" 
+# right = "Tom"
+# right_type = "string"
+
+#e.g if the condition is instructor_salary > '12000.00'
+# left = "instructor_salary"
+# left_type = "column" 
+# relation = ">" 
+# right = "12000.00"
+# right_type = "float"
+
+#e.g if the condition is instructor_salary > '12000'
+# left = "instructor_salary"
+# left_type = "column" 
+# relation = ">" 
+# right = "12000"
+# right_type = "int"
+
+#The python data type of "left" and "right" is string, but their actual data type is stored in "left_type" and "right_type"
