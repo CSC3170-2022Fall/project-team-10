f464ab46b8575ed6be048bba4bab33259fa5f826
Gila-6 <115449867+Gila-6@users.noreply.github.com>
2022-12-13 16:02:16 +0800

    Update Database.py

--- option3/Database.py
+++ option3/Database.py
@@ -1,18 +1,43 @@
 class Database:
-    tables = [] # a list of class Table
-    def do_command(command): # command: class Command
-        return 
+    def __init__(self):
+        self.tables = [] # a list of class Table
+
+    def do_command(self, command): # command: class Command
+        if (command.type == "create table"):
+            pass
+        elif (command.type == "create table as"):
+            pass
+        elif (command.type == "load"):
+            pass
+        elif (command.type == "store"):
+            pass
+        elif (command.type == "insert"):
+            pass
+        elif (command.type == "print"):
+            pass
+        elif ((command.type == "quit") or (command.type == "exit")):
+            pass
+        elif (command.type == "select"):
+            pass
+        return
 
     def create_table():
+        pass
     
-    def load(name): # name: string
+    def load(self, name): # name: string
+        pass
     
-    def store(name): # name: string
+    def store(self, name): # name: string
+        pass
     
-    def insert(name): # name: string
+    def insert(self, name): # name: string
+        pass
     
-    def print(name): # name: string
+    def print(self, name): # name: string
+        pass
     
-    def quit():
+    def quit(self):
+        pass
     
-    def select(column, table, condition): # column: a list names, table: a list of table names, condition: a list of class condition
+    def select(self, column, table, condition): # column: a list names, table: a list of table names, condition: a list of class condition
+        pass
