566a5260d2a062bda725c2ec83445a8eeed82a8e
Gila-6 <115449867+Gila-6@users.noreply.github.com>
2022-12-13 15:47:39 +0800

    Update Table.py

--- option3/Table.py
+++ option3/Table.py
@@ -1,5 +1,8 @@
 class Table:
-    name = ""
-    column = [] # a list of column names
-    data = [] # a list of rows
+    def __init__(self):
+        self.name = ""
+        self.column = [] # a list of column names
+        self.data = [] # a list of rows
+    
     def join():
+        pass
