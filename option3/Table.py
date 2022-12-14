e4d3024c0aba21213262695ab55fd2a722fcc9f8
Gila-6 <115449867+Gila-6@users.noreply.github.com>
2022-12-14 15:00:07 +0800

    Update Table.py

--- option3/Table.py
+++ option3/Table.py
@@ -1,12 +1,13 @@
-class Table:
+class Table:    
+    #  Possible value for coltype: int(), float(,), char(), varchar(), text
     def __init__(self):
         self.name = ""
         self.column = [] # a list of column names
-        self.data = [] # a list of rows
+        self.data = [] # a list of string rows
         self.coltype = [] # type of each column
         self.notnull = [] # notnull: 1, default: 0
-        self.dflt_value = [] # default value. default is None
-        self.pk = [] # primary key: 1, default:0 
+        self.dflt_value = [] # default value
+        self.pk = [] # primary key: 1, default: 0
     
     def join():
         pass
