5b96ded5db1591cd170031765c6f51b7a1bf88ee
Barrxxx <97070890+Barrxxx@users.noreply.github.com>
2022-12-24 19:04:26 +0800

    Update Table.py

--- option3/Table.py
+++ option3/Table.py
@@ -29,12 +29,12 @@ class Table:
             new_table.pk.append(table_2.pk[i])            
 
 
+
         for i in range(len(table_1.column)):
             for j in range(len(table_2.column)):
                 if table_1.column[i] == table_2.column[j]:
                     flag_1.append(i)
                     flag_2.append(j)
-
 
         if len(flag_1) == 0:
             # Outer Join
@@ -46,10 +46,15 @@ class Table:
 
         else:
             # Natural Join
-            for i in len(table_1.data):  
-                for j in len(table_2.data): 
-                    for k in len(flag_1):
-                        if table_1.data[i][flag_1[k]] == table_2.data[j][flag_2[k]]: # Determine if the elements are the
-                            new_data = table_1[i]+table_2[j]
+            for i in range(len(table_1.data)): 
+                t1_data = table_1.data[i] 
+                for j in range(len(table_2.data)):
+                    t2_data = table_2.data[j] 
+                    for k in range(len(flag_1)):
+                        if t1_data[flag_1[k]] == t2_data[flag_2[k]]: # Determine if the elements are the
+                            new_data = t1_data+t2_data
                             new_table.data.append(new_data)
+            # print(new_table.column)
+            # for i in range(len(new_table.data)):
+            #     print(new_table.data[i])
             return new_table
