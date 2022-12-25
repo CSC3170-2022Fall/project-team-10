8abbb7ae404c4138921a41e3fdfcece81cf55d0c
Gila-6 <115449867+Gila-6@users.noreply.github.com>
2022-12-25 21:43:48 +0800

    Update Table.py

--- option3/Table.py
+++ option3/Table.py
@@ -1,13 +1,8 @@
 class Table:    
-    #  Possible value for coltype: int(), numeric(,), char(), varchar(), text
     def __init__(self):
         self.name = ""
         self.column = [] # a list of column names
         self.data = [] # a list of string rows
-        self.coltype = [] # type of each column
-        self.notnull = [] # notnull: 1, default: 0
-        self.dflt_value = [] # default value
-        self.pk = [] # primary key: 1, default: 0
     
     def join(self,table): # This part should return a new table by Cartesian product of two table
         table_1 = self
